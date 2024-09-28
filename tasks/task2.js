114149721f16af247ec7576f004c964cbb979357
Krasnosilska Diana <krrr.diana.zno@gmail.com>
2024-09-28 15:51:38 +0300

    .

--- tasks/task2.js
+++ tasks/task2.js
@@ -2,8 +2,20 @@
 //  при цьому пропускаючи всі цифри.
 
 function reverseWithoutNumbers(str) {
-  // Ваш код тут
+  let nonNumericChars = '';
+  for (let i = 0; i < str.length; i++) {
+    if (isNaN(parseInt(str[i]))) { // Если символ не число
+      nonNumericChars += str[i];
+    }
+  }
+  let reversed = '';
+  for (let i = nonNumericChars.length - 1; i >= 0; i--) {
+    reversed += nonNumericChars[i];
+  }
+  
+  return reversed;
 }
+
 
 console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
 console.log(reverseWithoutNumbers("abc123xyz"));       // Виведе: "zyxabc"
--- tasks/task4.js
+++ tasks/task4.js
@@ -1,8 +1,15 @@
 // Задача: Напишіть функцію findMax, яка приймає масив чисел і повертає найбільше число в масиві.
 
 function findMax(numbers) {
-  //Ваш код
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
 }
+
 
 // Приклади використання:
 console.log(findMax([3, 5, 7, 2, 8])); // Виведе: 8
